b3eb242fb6bb586baac3ffba7dafbf97bdb8e9cd
liushuang <liushuanggo@gmail.com>
2020-12-14 00:02:28 +0800

    青蛙跳台阶问题加入动态规划算法

--- Leetcode/src/main/java/com/ls/leetcode/question/FrogJumpStep.java
+++ Leetcode/src/main/java/com/ls/leetcode/question/FrogJumpStep.java
@@ -9,8 +9,8 @@ package com.ls.leetcode.question;
 public class FrogJumpStep {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 6; ++i) {
-            System.out.println(frogJumpStep(i));
+        for (int i = 1; i < 60; ++i) {
+            System.out.println(frogJumpStep2(i));
         }
     }
 
@@ -24,5 +24,18 @@ public class FrogJumpStep {
         }
 
         return frogJumpStep(n - 1) + frogJumpStep(n - 2);
+    }
+
+    static int frogJumpStep2(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000_000_007;
+        }
+        return dp[n];
     }
 }
