fd9f74a66e295a336e1a146be5a8d2e57aab09df
lsc1993 <liushuanggo@gmail.com>
2021-01-06 23:12:24 +0800

    无重复字符的最长子串提交滑动窗口算法

--- Leetcode/src/main/java/com/ls/leetcode/question/LongestSubstringWithoutRepeatingCharacters.java
+++ Leetcode/src/main/java/com/ls/leetcode/question/LongestSubstringWithoutRepeatingCharacters.java
@@ -2,6 +2,8 @@ package com.ls.leetcode.question;
 
 import com.ls.leetcode.util.Log;
 
+import java.util.HashSet;
+
 /**
  * Longest Substring Without Repeating Characters
  *
@@ -59,14 +61,32 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return tempLen > subStrLen? tempLen : subStrLen;
     }
 
-    public static void testLengthOfLongestSubstring() {
+    public static int lengthOfLongestSubstring2(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int len = s.length();
+        int ans = 0;
+        int rs = 0;
+        for (int i = 0; i < len; ++i) {
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            while (rs < len && !set.contains(s.charAt(rs))) {
+                set.add(s.charAt(rs));
+                ++ rs;
+            }
+            ans = Math.max(rs - i, ans);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
         String s1 = "abcabcbb";
         String s2 = "wobgrovw";
         String s3 = "ggububgvfk";
 
-        int l1 = lengthOfLongestSubstring(s1);
-        int l2 = lengthOfLongestSubstring(s2);
-        int l3 = lengthOfLongestSubstring(s3);
+        int l1 = lengthOfLongestSubstring2(s1);
+        int l2 = lengthOfLongestSubstring2(s2);
+        int l3 = lengthOfLongestSubstring2(s3);
 
         Log.logLn("result:" + s1 + "->" + l1 + " " + s2 + "->" + l2 + " " + s3 + "->" + l3);
     }
