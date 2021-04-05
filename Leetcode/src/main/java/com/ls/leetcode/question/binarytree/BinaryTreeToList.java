5b4bf77fd6b70345951e80481eef4cab0057406f
lsc1993 <liushuanggo@gmail.com>
2021-04-05 23:05:58 +0800

    二叉树展开为链表

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/binarytree/BinaryTreeToList.java
@@ -0,0 +1,32 @@
+package com.ls.leetcode.question.binarytree;
+
+/**
+ * 二叉树展开为链表
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ *
+ * @link https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-5
+ */
+public class BinaryTreeToList {
+
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode next = curr.left;
+                TreeNode predecessor = next;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
+                }
+                predecessor.right = curr.right;
+                curr.left = null;
+                curr.right = next;
+            }
+            curr = curr.right;
+        }
+    }
+}
