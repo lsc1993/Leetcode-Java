66752abb68a9b0c3e8fd6be7db9252fabf6dd6c1
lsc1993 <liushuanggo@gmail.com>
2021-04-11 17:13:54 +0800

    路径总和 II

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/binarytree/FindDuplicateSubTree.java
@@ -0,0 +1,40 @@
+package com.ls.leetcode.question.binarytree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 寻找重复的子树
+ * 给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。
+ * 两棵树重复是指它们具有相同的结构以及相同的结点值。
+ *
+ * @link https://leetcode-cn.com/problems/find-duplicate-subtrees/
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-11
+ */
+public class FindDuplicateSubTree {
+
+    Map<String, Integer> count = new HashMap<>();
+    List<TreeNode> ans = new ArrayList<>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        collect(root);
+        return ans;
+    }
+
+    private String collect(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+
+        String serial = root.val + "," + collect(root.left) + "," + collect(root.right);
+        count.put(serial, count.getOrDefault(serial, 0) + 1);
+        if (count.get(serial) == 2) {
+            ans.add(root);
+        }
+        return serial;
+    }
+}
