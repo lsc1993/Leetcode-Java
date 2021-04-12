8a098d96b41789bd05733b9f7aaf3ef5ce097912
lsc1993 <liushuanggo@gmail.com>
2021-04-12 23:07:29 +0800

    另一个树的子树

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/binarytree/SubTreeOfAnotherTree.java
@@ -0,0 +1,35 @@
+package com.ls.leetcode.question.binarytree;
+
+/**
+ * 另一个树的子树
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。
+ * s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ *
+ * {}@link https://leetcode-cn.com/problems/subtree-of-another-tree
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-11
+ */
+public class SubTreeOfAnotherTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return dfs(s, t);
+    }
+
+    public boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return check(s, t) || dfs(s.left, t) || dfs(s.right, t);
+    }
+
+    public boolean check(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return check(s.left, t.left) && check(s.right, t.right);
+    }
+}
