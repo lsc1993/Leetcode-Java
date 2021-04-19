6be9927ebe62d21551c87d356c81a86186db97f2
lsc1993 <liushuanggo@gmail.com>
2021-04-19 22:51:18 +0800

    复原 IP 地址

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/recall/RestoreIpAddress.java
@@ -0,0 +1,79 @@
+package com.ls.leetcode.question.recall;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 复原 IP 地址
+ * 给定一个只包含数字的字符串，用以表示一个 IP 地址，返回所有可能从 s 获得的 有效 IP 地址 。你可以按任何顺序返回答案。
+ * 有效 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+ * 例如："0.1.2.201" 和 "192.168.1.1" 是 有效 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效 IP 地址。
+ *
+ * {}@link https://leetcode-cn.com/problems/restore-ip-addresses
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-19
+ */
+public class RestoreIpAddress {
+
+    public List<String> restoreIpAddresses(String s) {
+        int len = s.length();
+        List<String> res = new ArrayList<>();
+        if (len < 4 || len > 12) {
+            return res;
+        }
+
+        Deque<String> path = new ArrayDeque<>(4);
+        int splitTimes = 0;
+        dfs(s, len, splitTimes, 0, path, res);
+        return res;
+    }
+
+    private int judgeIfIpSegment(String s, int left, int right) {
+        int len = right - left + 1;
+
+        if (len > 1 && s.charAt(left) == '0') {
+            return -1;
+        }
+
+        int res = 0;
+        for (int i = left; i <= right; i++) {
+            res = res * 10 + s.charAt(i) - '0';
+        }
+
+        if (res > 255) {
+            return -1;
+        }
+        return res;
+    }
+
+    private void dfs(String s, int len, int split, int begin, Deque<String> path, List<String> res) {
+        if (begin == len) {
+            if (split == 4) {
+                res.add(String.join(".", path));
+            }
+            return;
+        }
+
+        // 看到剩下的不够了，就退出（剪枝），len - begin 表示剩余的还未分割的字符串的位数
+        if (len - begin < (4 - split) || len - begin > 3 * (4 - split)) {
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (begin + i >= len) {
+                break;
+            }
+
+            int ipSegment = judgeIfIpSegment(s, begin, begin + i);
+            if (ipSegment != -1) {
+                // 在判断是 ip 段的情况下，才去做截取
+                path.addLast(ipSegment + "");
+                dfs(s, len, split + 1, begin + i + 1, path, res);
+                path.removeLast();
+            }
+        }
+    }
+}
