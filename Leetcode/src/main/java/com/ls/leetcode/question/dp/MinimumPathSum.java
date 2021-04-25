54638d5abdd4e797b22b746fff7f126ff12a532d
lsc1993 <liushuanggo@gmail.com>
2021-04-25 22:11:04 +0800

    不同路径-补充动态规划解法

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/dp/MinimumPathSum.java
@@ -0,0 +1,57 @@
+package com.ls.leetcode.question.dp;
+
+/**
+ * 最小路径和
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ *
+ * {}@link https://leetcode-cn.com/problems/minimum-path-sum/
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-25
+ */
+public class MinimumPathSum {
+
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (i > 0 && j < 1) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+                if (i < 1 && j > 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                }
+                if (i > 0 && j > 0) {
+                    dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+                }
+            }
+        }
+
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+
+    // 更快
+    public int minPathSum2(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rows = grid.length, columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+}
