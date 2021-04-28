4dac6de1988a826c119e564354946e983e9e27b9
lsc1993 <liushuanggo@gmail.com>
2021-04-29 00:28:35 +0800

    整数拆分

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/dp/IntegerBreak.java
@@ -0,0 +1,26 @@
+package com.ls.leetcode.question.dp;
+
+/**
+ * 整数拆分
+ * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+ *
+ * {}@link https://leetcode-cn.com/problems/integer-break/
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-29
+ */
+public class IntegerBreak {
+
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                dp[i] = Math.max(dp[i], Math.max((i - j) * j, dp[i - j] * j));
+            }
+        }
+
+        return dp[n];
+    }
+}
