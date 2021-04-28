4098357a699b54671668d22bd9769b0ec9332c51
lsc1993 <liushuanggo@gmail.com>
2021-04-28 23:54:40 +0800

    零钱兑换

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/dp/CoinChange.java
@@ -0,0 +1,34 @@
+package com.ls.leetcode.question.dp;
+
+import java.util.Arrays;
+
+/**
+ * 零钱兑换
+ * 给定不同面额的硬币 coins 和一个总金额 amount。
+ * 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+ * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ * {}@link https://leetcode-cn.com/problems/coin-change
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-4-28
+ */
+public class CoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; ++i) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
