054cd8dcc5370ad8a87facff2ef477bd9284a43b
liushuang <liushuanggo@gmail.com>
2021-05-13 22:22:31 +0800

    分类题目

--- Leetcode/src/main/java/com/ls/leetcode/question/array/TwoSum.java
+++ Leetcode/src/main/java/com/ls/leetcode/question/array/TwoSum.java
@@ -1,4 +1,4 @@
-package com.ls.leetcode.question;
+package com.ls.leetcode.question.array;
 
 import com.ls.leetcode.util.Log;
 
--- Leetcode/src/main/java/com/ls/leetcode/question/math/BinaryNumberOfOne.java
+++ Leetcode/src/main/java/com/ls/leetcode/question/math/BinaryNumberOfOne.java
@@ -1,4 +1,4 @@
-package com.ls.leetcode.question;
+package com.ls.leetcode.question.math;
 
 /**
  * 二进制中1的个数
