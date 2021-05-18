ed9680fa3a724d80ff95ac106e504b155302ad49
liushuang <liushuanggo@gmail.com>
2021-05-18 23:57:55 +0800

    划分字母区间

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/doubleponiter/PartitionLabels.java
@@ -0,0 +1,34 @@
+package com.ls.leetcode.question.doubleponiter;
+
+import java.util.*;
+
+/**
+ * 划分字母区间
+ * 字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。
+ * 返回一个表示每个字符串片段的长度的列表。
+ *
+ * https://leetcode-cn.com/problems/partition-labels/
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-5-18
+ */
+public class PartitionLabels {
+
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+}
