726243409081a9303b11c1dec230e7a88cc42b5b
liushuang <liushuanggo@gmail.com>
2021-05-23 16:01:06 +0800

    整数反转

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/math/ReverseInteger.java
@@ -0,0 +1,28 @@
+package com.ls.leetcode.question.math;
+
+/**
+ * 整数反转
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ *
+ * https://leetcode-cn.com/problems/reverse-integer
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-5-23
+ */
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+        return rev;
+    }
+}
