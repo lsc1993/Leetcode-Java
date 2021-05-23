9caf16b859033080ad4bb4b3b8cdb8c80411c231
liushuang <liushuanggo@gmail.com>
2021-05-23 20:47:41 +0800

    删除二叉搜索树中的节点

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/binarytree/DeleteNodeInBST.java
@@ -0,0 +1,47 @@
+package com.ls.leetcode.question.binarytree;
+
+/**
+ * 删除二叉搜索树中的节点
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，
+ * 并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+ * 一般来说，删除节点可分为两个步骤：
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ * 说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+ *
+ * https://leetcode-cn.com/problems/delete-node-in-a-bst
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-5-23
+ */
+public class DeleteNodeInBST {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            TreeNode minNode = getMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        }
+        return root;
+    }
+
+    private TreeNode getMin(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
