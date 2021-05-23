1ee5f3c9db205529a3aec1244633648298cec0ec
liushuang <liushuanggo@gmail.com>
2021-05-23 14:24:21 +0800

    下一个排列

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/array/NextPermutation.java
@@ -0,0 +1,45 @@
+package com.ls.leetcode.question.array;
+
+/**
+ * 下一个排列
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 必须 原地 修改，只允许使用额外常数空间。
+ *
+ * https://leetcode-cn.com/problems/next-permutation
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-5-23
+ */
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
