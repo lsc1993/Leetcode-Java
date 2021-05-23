1eadd4cfe319757a0c9832bf8e4d0ffb701818bd
liushuang <liushuanggo@gmail.com>
2021-05-23 19:57:04 +0800

    用栈实现队列

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/design/MyQueue.java
@@ -0,0 +1,52 @@
+package com.ls.leetcode.question.design;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 用栈实现队列
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）
+ *
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ *
+ * @author liushuanggo@gmail.com
+ * @since 2021-5-23
+ */
+class MyQueue {
+
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new LinkedList<Integer>();
+        outStack = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void in2out() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
+    }
+}
