599d50e17c68f1769754d9b4b6eac5f0393ac697
liushuanggo <liushuanggo@gmail.com>
2022-07-23 23:59:38 +0800

    二进制求和

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/math/AddBinary.java
@@ -0,0 +1,32 @@
+package com.ls.leetcode.question.math;
+
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ * <a href="https://leetcode.cn/problems/add-binary/">二进制求和</a>
+ *
+ * @author liushuanggo
+ * @since 2022-07-23
+ */
+public class AddBinary {
+
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+}
