837b70e262c7cb2a0bf52f1a9ae090f325d8834d
liushuanggo <liushuanggo@gmail.com>
2022-08-01 23:57:29 +0800

    Z 字形变换

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/string/ZConvert.java
@@ -0,0 +1,40 @@
+package com.ls.leetcode.question.string;
+
+/**
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * <a href="https://leetcode.cn/problems/zigzag-conversion/">Z 字形变换</a>
+ *
+ * @author liushuanggo
+ * @since 2022-08-01
+ */
+public class ZConvert {
+
+    public String convert(String s, int numRows) {
+        int n = s.length(), r = numRows;
+        if (r == 1 || r >= n) {
+            return s;
+        }
+        int t = r * 2 - 2;
+        int c = (n + t - 1) / t * (r - 1);
+        char[][] mat = new char[r][c];
+        for (int i = 0, x = 0, y = 0; i < n; ++i) {
+            mat[x][y] = s.charAt(i);
+            if (i % t < r - 1) {
+                ++x; // 向下移动
+            } else {
+                --x;
+                ++y; // 向右上移动
+            }
+        }
+        StringBuffer ans = new StringBuffer();
+        for (char[] row : mat) {
+            for (char ch : row) {
+                if (ch != 0) {
+                    ans.append(ch);
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
