a636da9a681d14ffa1d4d0d9c431855f8025f0f7
liushuanggo <liushuanggo@gmail.com>
2022-08-04 00:04:18 +0800

    和为 K 的子数组

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/array/SubArraySum.java
@@ -0,0 +1,42 @@
+package com.ls.leetcode.question.array;
+
+import java.util.HashMap;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+ *
+ * <a href="https://leetcode.cn/problems/subarray-sum-equals-k/">和为 K 的子数组</a>
+ *
+ * @author liushuanggo
+ * @since 2022-08-03
+ */
+public class SubArraySum {
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; ++start) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap< Integer, Integer > mp = new HashMap<> ();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
