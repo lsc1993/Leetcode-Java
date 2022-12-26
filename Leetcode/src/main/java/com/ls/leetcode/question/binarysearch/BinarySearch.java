16e05582342c9cb56228a76992fe017ccacfae75
liushuang <ls348097@alibaba-inc.com>
2022-12-26 11:33:45 +0800

    二分查找

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/binarysearch/BinarySearch.java
@@ -0,0 +1,30 @@
+package com.ls.leetcode.question.binarysearch;
+
+/**
+ * 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ *
+ * https://leetcode.cn/problems/binary-search
+ *
+ * @author liushuang
+ * @since 2022-12-26
+ */
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
