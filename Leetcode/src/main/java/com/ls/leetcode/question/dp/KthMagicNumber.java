50f10fa76a7cdd2bfc99105b6b003513a4cbe4ad
liushuang <ls348097@alibaba-inc.com>
2023-02-23 00:44:18 +0800

    第 k 个数

---
+++ Leetcode/src/main/java/com/ls/leetcode/question/dp/KthMagicNumber.java
@@ -0,0 +1,33 @@
+package com.ls.leetcode.question.dp;
+
+/**
+ * 有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。
+ * 注意，不是必须有这些素因子，而是必须不包含其他的素因子。
+ * 例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+ *
+ * https://leetcode.cn/problems/get-kth-magic-number-lcci
+ *
+ * @author liushuang
+ * @since 2023-02-23
+ */
+public class KthMagicNumber {
+    public int getKthMagicNumber(int k) {
+        int[] dp = new int[k + 1];
+        dp[1] = 1;
+        int p3 = 1, p5 = 1, p7 = 1;
+        for (int i = 2; i <= k; i++) {
+            int num3 = dp[p3] * 3, num5 = dp[p5] * 5, num7 = dp[p7] * 7;
+            dp[i] = Math.min(Math.min(num3, num5), num7);
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+            if (dp[i] == num7) {
+                p7++;
+            }
+        }
+        return dp[k];
+    }
+}
