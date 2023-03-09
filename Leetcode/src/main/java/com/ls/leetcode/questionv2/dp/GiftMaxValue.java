aed0f7f05c88f83878af70333022dd927e339c18
liushuanggo <liushuanggo@gmail.com>
2023-03-10 00:25:14 +0800

    礼物的最大价值

---
+++ Leetcode/src/main/java/com/ls/leetcode/questionv2/dp/GiftMaxValue.java
@@ -0,0 +1,29 @@
+package com.ls.leetcode.questionv2.dp;
+
+/**
+ * 礼物的最大价值
+ * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
+ * 你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
+ * 给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+ *
+ * https://leetcode.cn/problems/li-wu-de-zui-da-jie-zhi-lcof
+ *
+ * @author liushuanggo
+ * @since 2023-03-10
+ */
+public class GiftMaxValue {
+
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+                if(i == 0) grid[i][j] += grid[i][j - 1] ;
+                else if(j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.max(grid[i][j - 1], grid[i - 1][j]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
