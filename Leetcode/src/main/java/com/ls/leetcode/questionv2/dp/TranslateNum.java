62754ddb11865a567fe232561df8eb2d7adb1311
liushuanggo <liushuanggo@gmail.com>
2023-03-13 23:23:06 +0800

    把数字翻译成字符串

---
+++ Leetcode/src/main/java/com/ls/leetcode/questionv2/dp/TranslateNum.java
@@ -0,0 +1,28 @@
+package com.ls.leetcode.questionv2.dp;
+
+/**
+ * 把数字翻译成字符串
+ *
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，
+ * 11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，
+ * 用来计算一个数字有多少种不同的翻译方法。
+ *
+ * https://leetcode.cn/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof
+ *
+ * @author liushuanggo
+ * @since 2023-03-13
+ */
+public class TranslateNum {
+
+    public int translateNum(int num) {
+        String s = String.valueOf(num);
+        int a = 1, b = 1;
+        for(int i = 2; i <= s.length(); i++) {
+            String tmp = s.substring(i - 2, i);
+            int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+            b = a;
+            a = c;
+        }
+        return a;
+    }
+}
