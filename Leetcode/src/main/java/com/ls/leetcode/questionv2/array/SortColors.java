108248bc5e76e9fe088010047a246b7ef28a6e75
liushuanggo <liushuanggo@gmail.com>
2023-03-26 23:43:05 +0800

    颜色分类

---
+++ Leetcode/src/main/java/com/ls/leetcode/questionv2/array/SortColors.java
@@ -0,0 +1,37 @@
+package com.ls.leetcode.questionv2.array;
+
+/**
+ * 颜色分类
+ * 给定一个包含红色、白色和蓝色、共n 个元素的数组nums，
+ * 原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 我们使用整数 0、1 和 2 分别表示红色、白色和蓝色。
+ * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+ *
+ * https://leetcode.cn/problems/sort-colors
+ *
+ * @author liushuanggo
+ * @since 2023-03-26
+ */
+public class SortColors {
+
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int ptr = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+        for (int i = ptr; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+    }
+}
