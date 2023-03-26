275f2fa4e7afcf3891cbd855f6fa42a19023c3bf
liushuanggo <liushuanggo@gmail.com>
2023-03-26 23:28:38 +0800

    字母异位词分组

---
+++ Leetcode/src/main/java/com/ls/leetcode/questionv2/string/GroupAnagrams.java
@@ -0,0 +1,29 @@
+package com.ls.leetcode.questionv2.string;
+
+import java.util.*;
+
+/**
+ * 字母异位词分组
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+ *
+ * https://leetcode.cn/problems/group-anagrams
+ *
+ * @author liushuanggo
+ * @since 2023-03-26
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
