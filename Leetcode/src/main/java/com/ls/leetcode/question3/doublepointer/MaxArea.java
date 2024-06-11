6ad66dac3ce553b30fbb044b907da18dfd0c57b9
liushuanggo <liushuanggo@gmail.com>
2024-06-11 22:42:42 +0800

    盛最多水的容器

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/doublepointer/MaxArea.java
@@ -0,0 +1,20 @@
+package com.ls.leetcode.question3.doublepointer;
+
+public class MaxArea {
+
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
+}
