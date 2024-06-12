37f9bcdebaf64584510ecbbb43544b3beac477d5
liushuanggo <liushuanggo@gmail.com>
2024-06-12 22:42:04 +0800

    删除有序数组中的重复项 II

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/doublepointer/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package com.ls.leetcode.question3.doublepointer;
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+        if (len <= 2) {
+            return len;
+        }
+        int slow = 2, fast = 2;
+        while (fast < len) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
