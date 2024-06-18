50d4f607f961a81e2af34076f75b4f9264ae7022
liushuanggo <liushuanggo@gmail.com>
2024-06-18 23:37:17 +0800

    找到 K 个最接近的元素

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/doublepointer/FindClosestElements.java
@@ -0,0 +1,40 @@
+package com.ls.leetcode.question3.doublepointer;
+
+import java.util.*;
+
+public class FindClosestElements {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int right = binarySearch(arr, x);
+        int left = right - 1;
+        while (k-- > 0) {
+            if (left < 0) {
+                right++;
+            } else if (right >= arr.length) {
+                left--;
+            } else if (x - arr[left] <= arr[right] - x) {
+                left--;
+            } else {
+                right++;
+            }
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = left + 1; i < right; i++) {
+            ans.add(arr[i]);
+        }
+        return ans;
+    }
+
+    public int binarySearch(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
