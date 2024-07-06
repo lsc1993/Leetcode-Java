2493d86a0850fe7bf8de14cd8f5e16f1b697a9d0
liushuanggo <liushuanggo@gmail.com>
2024-07-06 17:44:07 +0800

    跳跃游戏 II

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/CanJumpII.java
@@ -0,0 +1,19 @@
+package com.ls.leetcode.question3.interview150;
+
+public class CanJumpII {
+
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
