a44a9ac00f9c189e6154b2e8bb6d904f4b79d9ae
liushuanggo <liushuanggo@gmail.com>
2024-07-06 16:31:45 +0800

    轮转数组

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/RotateArray.java
@@ -0,0 +1,13 @@
+package com.ls.leetcode.question3.interview150;
+
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+}
