608b25729652fc63a53ccc22f735fdf74db8e6de
liushuanggo <liushuanggo@gmail.com>
2024-07-14 23:28:35 +0800

    两数之和 II - 输入有序数组

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/TwoSumII.java
@@ -0,0 +1,20 @@
+package com.ls.leetcode.question3.interview150;
+
+public class TwoSumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
