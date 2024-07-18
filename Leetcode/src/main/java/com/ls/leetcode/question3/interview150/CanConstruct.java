084ad4807bad108e251fb912e2f6394408eea758
liushuanggo <liushuanggo@gmail.com>
2024-07-18 23:01:46 +0800

    赎金信

--- Leetcode/src/main/java/com/ls/leetcode/question3/interview150/CanConstruct.java
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/CanConstruct.java
@@ -28,4 +28,21 @@ public class CanConstruct {
         }
         return true;
     }
+
+    public boolean canConstruct1(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (char c : magazine.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            cnt[c - 'a']--;
+            if(cnt[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
