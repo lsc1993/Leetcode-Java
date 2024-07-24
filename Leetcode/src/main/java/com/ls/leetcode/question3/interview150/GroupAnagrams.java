c95be6eb2ea91d7227e78c78c979c09c2cf2a6d0
liushuanggo <liushuanggo@gmail.com>
2024-07-24 23:24:10 +0800

    字母异位词分组

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/GroupAnagrams.java
@@ -0,0 +1,22 @@
+package com.ls.leetcode.question3.interview150;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
