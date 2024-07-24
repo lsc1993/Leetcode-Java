d5b3f04e29fd0003c133cd16a273ac41d058f2ed
liushuanggo <liushuanggo@gmail.com>
2024-07-24 23:40:27 +0800

    存在重复元素 II

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/ContainsNearbyDuplicate.java
@@ -0,0 +1,23 @@
+package com.ls.leetcode.question3.interview150;
+
+import java.util.HashMap;
+
+public class ContainsNearbyDuplicate {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int index = map.get(nums[i]);
+                if (i - index <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
