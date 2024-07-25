0d6d1116722b2b2f47bdd85a182d475af14f5192
liushuanggo <liushuanggo@gmail.com>
2024-07-25 23:15:50 +0800

    最小栈

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/MinStack.java
@@ -0,0 +1,34 @@
+package com.ls.leetcode.question3.interview150;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
