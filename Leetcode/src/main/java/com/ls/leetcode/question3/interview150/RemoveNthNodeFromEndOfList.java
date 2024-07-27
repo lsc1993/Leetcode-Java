35cea19106bc29b4088085ed68509fd1c42920c9
liushuanggo <liushuanggo@gmail.com>
2024-07-27 22:14:32 +0800

    删除链表的倒数第 N 个结点

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,22 @@
+package com.ls.leetcode.question3.interview150;
+
+import com.ls.leetcode.question.list.ListNode;
+
+public class RemoveNthNodeFromEndOfList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+
+    }
+}
