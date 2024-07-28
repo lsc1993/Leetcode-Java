e651dad6c0c01353a49190c4ee5f0620cf71b225
liushuanggo <liushuanggo@gmail.com>
2024-07-28 23:13:58 +0800

    二叉树的最大深度

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+package com.ls.leetcode.question3.interview150;
+
+import com.ls.leetcode.question.binarytree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
