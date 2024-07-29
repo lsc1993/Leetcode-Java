30cae1f10f2620f48ed7c54da4d8f456f09bd9e8
liushuanggo <liushuanggo@gmail.com>
2024-07-29 22:28:30 +0800

    相同的树

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/SameTree.java
@@ -0,0 +1,29 @@
+package com.ls.leetcode.question3.interview150;
+
+import com.ls.leetcode.question.binarytree.TreeNode;
+
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q != null) {
+            return false;
+        }
+
+        if (p != null && q == null) {
+            return false;
+        }
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        return left && right;
+    }
+}
