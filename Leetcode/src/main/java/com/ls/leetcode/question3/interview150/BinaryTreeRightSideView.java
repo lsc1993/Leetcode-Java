7be8ebb3ae9f1efa71c8b0624fc83ec022440395
liushuanggo <liushuanggo@gmail.com>
2024-08-04 22:43:29 +0800

    二叉树的右视图

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/BinaryTreeRightSideView.java
@@ -0,0 +1,36 @@
+package com.ls.leetcode.question3.interview150;
+
+import com.ls.leetcode.question.binarytree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i == sz - 1) {
+                    result.add(node.val);
+                }
+            }
+        }
+        return result;
+    }
+}
