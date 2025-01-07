351a687814d6e019682623c15b4341ee78cb2464
liushuanggo <liushuanggo@gmail.com>
2025-01-07 23:25:29 +0800

    验证二叉搜索树

---
+++ Leetcode/src/main/java/com/ls/leetcode/question3/interview150/ValidBST.java
@@ -0,0 +1,20 @@
+package com.ls.leetcode.question3.interview150;
+
+import com.ls.leetcode.question.binarytree.TreeNode;
+
+public class ValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+}
